6d880dd6f3cc6c1ec31288742dbbf641c90438b1
8770720 <92971190+8770720@users.noreply.github.com>
2025-06-05 23:14:31 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://english-learning-assistant-omega.vercel.app"],  # 你的前端網址
+    allow_origins=["https://english-learning-assistant-omega.vercel.app","https://www.google.com"],  # 你的前端網址
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
